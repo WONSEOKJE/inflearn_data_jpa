dfc7b36b52192b9205b271e4d71b6ae39989e64c
WONSEOKJE <nicholas4744@gmail.com>
2023-01-25 14:47:21 +0900

    feat: 스프링 데이터 JPA 페이징과 정렬

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,6 +3,10 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
@@ -144,6 +148,33 @@ class MemberRepositoryTest {
 
         List<Member> result = memberRepository.findListByUsername("AAA");
         System.out.println("result = " + result);
+    }
+
+    @Test
+    public void paging() {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 10));
+        memberRepository.save(new Member("member3", 10));
+        memberRepository.save(new Member("member4", 10));
+        memberRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        //when
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+
+        Page<MemberDto> toMap = page.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+        //then
+        List<Member> content = page.getContent();
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(page.getTotalElements()).isEqualTo(5); //전체 데이터
+        assertThat(page.getNumber()).isEqualTo(0); //페이지 번호 가져오기
+        assertThat(page.getTotalPages()).isEqualTo(2); //전체 페이지 개수
+        assertThat(page.isFirst()).isTrue(); //첫 페이지인가?
+        assertThat(page.hasNext()).isTrue(); //다음 페이지가 있는가?
     }
 
 
