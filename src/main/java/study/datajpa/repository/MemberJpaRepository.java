1527471103f5a4bef3529ddbdf0b81c7aaf7e215
WONSEOKJE <nicholas4744@gmail.com>
2023-01-25 13:49:23 +0900

    feat: 순수 JPA 페이징과 정렬

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -38,7 +38,7 @@ public class MemberJpaRepository {
                 .getSingleResult();
     }
 
-    public Member find (Long id) {
+    public Member find(Long id) {
         return em.find(Member.class, id);
     }
 
@@ -51,7 +51,22 @@ public class MemberJpaRepository {
 
     public List<Member> findByUsername(String username) {
         return em.createNamedQuery("Member.findByUsername", Member.class)
-                        .setParameter("username", username)
-                        .getResultList();
+                .setParameter("username", username)
+                .getResultList();
     }
+
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
+
 }
